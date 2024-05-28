94cfe48076ff1b0939482f6f3675cd263f5036da
idc4k <christikonga83@gmail.com>
2024-05-28 16:34:15 +0100

    feat: start project passwordGenerate

---
+++ passwordGenerate/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './output.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-center text-red-500 text-3xl'>React password generate</h1>
+    </>
+  )
+}
+
+export default App
